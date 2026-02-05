29b78cc814d2d0f28d2510f99b2fca61d1534af9
Corrado-lab <corrado.cecchini27@gmail.com>
2026-02-05 16:58:49 +0100

    feat(doc): add pipeline ci/cd

---
+++ src/test/MainTests.java
@@ -0,0 +1,13 @@
+package it.unicam.coloni.hackhub ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+        // Se il codice arriva qui senza crashare, il test è passato!
+    }
+}
